84ed5575f7424caced3317eba2d26240c9bc6c73
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-01-19 22:05:55 +0400

    added movies list in Home Panel

--- src/panels/Home/Home.js
+++ src/panels/Home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { string, func, shape } from 'prop-types';
+import { string, number, func, shape } from 'prop-types';
+
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 import { Panel, Group, Div, PanelHeader, HeaderButton } from '@vkontakte/vkui';
 import Icon24Search from '@vkontakte/icons/dist/24/search';
 import Icon24Write from '@vkontakte/icons/dist/24/write';
 
 import { APP_NAME } from '../../config';
+
+import Loader from '../../components/Loader';
+import Card from '../../components/Card';
 
 import './Home.css';
 
@@ -27,6 +33,42 @@ const Home = ({ id, go, city }) => (
 				</button>
 			</Div>
 		</Group>
+
+		{(city && city.CityID) &&
+			<Group>
+				<Div>
+					<Query
+						query={gql`
+							query {
+								movies (CityID: ${city.CityID}) {
+									ObjectID,
+									Name,
+									Thumbnail,
+									Genre,
+									AgeRestriction
+								}
+							}
+						`}>
+						{({ loading, error, data }) => {
+							if (loading) return <Loader center={true} />;
+							if (error) return <p>Error :(</p>;
+
+							return <ul className="home__movies">
+								{data.movies.map(movie =>
+									<li className="home__movie" key={movie.ObjectID} onClick={go} data-to="movie">
+										<Card
+											cover={movie.Thumbnail}
+											title={movie.Name}
+											genre={movie.Genre}
+											ageRestriction={movie.AgeRestriction} />
+									</li>
+								)}
+							</ul>;
+						}}
+					</Query>
+				</Div>
+			</Group>}
+
 	</Panel>
 );
 
@@ -34,6 +76,7 @@ Home.propTypes = {
 	id: string.isRequired,
 	go: func.isRequired,
 	city: shape({
+		CityID: number.isRequired,
 		Name: string.isRequired
 	}),
 };
