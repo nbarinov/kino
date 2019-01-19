ae3a6ca2d70051a142b89d2dc61a05eecbe3d6c5
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-01-19 20:03:40 +0400

    added APP_NAME in config

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
 
+import { APP_NAME } from '../config';
+
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
+		<PanelHeader>{APP_NAME}</PanelHeader>
 		{fetchedUser &&
 		<Group title="User Data Fetched with VK Connect">
 			<ListItem
