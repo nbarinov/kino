0d0fb5d258d26ab17ff2c76cd8a20325f1c5285a
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-01-19 20:47:09 +0400

    setup city

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import connect from '@vkontakte/vkui-connect';
 import { View } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
+import sortByDistance from 'sort-by-distance';
+
 import Home from './panels/Home';
 
 import './App.css';
@@ -16,7 +18,9 @@ class App extends React.Component {
 
 	state = {
 		activePanel: 'home',
+		geo: null,
 		cities: null,
+		city: null,
 	};
 
 	componentWillMount() {
@@ -28,11 +32,43 @@ class App extends React.Component {
 	componentDidMount() {
 		connect.subscribe((e) => {
 			switch (e.detail.type) {
+				case 'VKWebAppGeodataResult':
+					this.setState({ geo: e.detail.data });
+					break;
 				default:
 					console.log(e.detail.type);
 			}
 		});
-		connect.send('VKWebAppGetUserInfo', {});
+
+		connect.send("VKWebAppGetGeodata", {});
+	}
+
+	componentDidUpdate() {
+		const { city, cities } = this.state;
+
+		// если получен список городов
+		if (!city && cities && cities.length > 0) {
+			const { geo } = this.state;
+
+			// если получена геопозиция
+			if (geo) {
+				const sortCities = sortByDistance(
+					{
+						Latitude: geo.lat,
+						Longitude: geo.long,
+					},
+					cities,
+					{
+						yName: 'Latitude',
+						xName: 'Longitude',
+					}
+				);
+
+				if (sortCities && sortCities.length > 0) {
+					this.setState({ city: sortCities[0] });
+				}
+			}
+		}
 	}
 
 	go = (e) => {
@@ -42,7 +78,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<View activePanel={this.state.activePanel}>
-				<Home id="home" go={this.go} />
+				<Home id="home" go={this.go} city={this.state.city} />
 			</View>
 		);
 	}
